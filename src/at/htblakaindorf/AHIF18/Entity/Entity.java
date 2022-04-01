7049c78c82cb78ecb140426d5d8f3a68328b0d16
Mxnu-jpg <82145853+Mxnu-jpg@users.noreply.github.com>
2022-04-01 13:40:56 +0200

    Delete src directory

--- src/at/htblakaindorf/AHIF18/Entity/Entity.java
+++
@@ -1,9 +0,0 @@
-package at.htblakaindorf.AHIF18.Entity;
-
-import java.awt.image.BufferedImage;
-
-public class Entity {
-
-    public double worldX, worldY;
-    public double speed;
-}
--- src/at/htblakaindorf/AHIF18/Ground/Tile.java
+++
@@ -1,9 +0,0 @@
-package at.htblakaindorf.AHIF18.Ground;
-
-import java.awt.image.BufferedImage;
-import java.io.BufferedReader;
-
-public class Tile {
-  public BufferedImage image;
-  boolean collision = false;
-}
